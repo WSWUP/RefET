a9ddcac635923b8da4b0030e6ffa2657a2f25875
Charles Morton <cgmorton@users.noreply.github.com>
2018-08-29 11:15:00 -0700

    Adding Python 3.7 to tests

--- setup.py
+++ setup.py
@@ -49,5 +49,6 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'],
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'],
 )
