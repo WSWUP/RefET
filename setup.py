7b49dd8a4b7c8b780cf1bd6b0f3042801a11c99c
Charles Morton <cgmorton@users.noreply.github.com>
2018-08-29 11:33:40 -0700

    Removing Python 3.7 tests
    Travis-CI doesn't currently support Python 3.7 unless you add custom entries to the yaml.

--- setup.py
+++ setup.py
@@ -50,5 +50,5 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7'],
+    ],
 )
